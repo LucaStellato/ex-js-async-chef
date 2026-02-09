cfef452487b0e751311b36e2028147589dd83262
LucaStellato <lucastellato435@gmail.com>
2026-02-09 14:24:56 +0100

    fetch complete

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 
 
+async function getChefBirthDay(id) {
+    const ricetta = await fetch(`https://dummyjson.com/recipes/${id}`)
+    const ricettaResponse = await ricetta.json();
+    console.log(ricettaResponse)
+    const chef = await fetch(`https://dummyjson.com/users/${ricettaResponse.userId}`)
+    const chefResponse = await chef.json();
+    console.log(chefResponse)
+    return chefResponse.birthDate
+
+}
 
 
+getChefBirthDay(1)
+    .then(birthday => console.log("Data di nascita dello chef:", birthday))
+    .catch(error => console.error("Errore:", error.message));
